7b0611cf6883bd975a89e60895084eaeecaec565
Taran Cacacho <tarancacacho@gmail.com>
2019-06-13 16:43:54 -0700

    Add premium column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_223135) do
+ActiveRecord::Schema.define(version: 2019_06_13_234301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_223135) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "premium"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
