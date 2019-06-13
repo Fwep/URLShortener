de9ca7cface24fc2264f9f9940198a54555d54cf
Taran Cacacho <tarancacacho@gmail.com>
2019-06-13 16:21:12 -0700

    Add instance method to list 5 most popular sites for a particular tag topic

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -1,3 +1,25 @@
 class TagTopic < ApplicationRecord
   validates :topic, uniqueness: true, presence: true 
+
+  has_many :taggings,
+    primary_key: :id,
+    foreign_key: :tag_id,
+    class_name: :Tagging
+
+  has_many :urls,
+    through: :taggings,
+    source: :url
+
+  def popular_links
+      links = self.urls.sort_by do |url|
+        url.num_clicks
+      end
+
+      link_counts = []
+      5.times do |i|
+        link = links[i]
+        link_counts.push([link.long_url, link.num_clicks]) unless link.nil?
+      end
+      link_counts
+  end
 end
