2e0d3038b68b8733c5ab88d9b082a6390ac5a7d5
Taran Cacacho <tarancacacho@gmail.com>
2019-06-12 19:05:54 -0700

    Add factory method to create and persist ShortenedUrl instance, and user association

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -2,5 +2,22 @@ class ShortenedUrl < ApplicationRecord
   validates :long_url, :short_url, presence: true, uniqueness: true
   validates :user_id, presence: true, uniqueness: false
 
-  
+  belongs_to :submitter,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+  def self.random_code
+    code = SecureRandom.urlsafe_base64
+
+    until !ShortenedUrl.exists?(code)
+      code = SecureRandom.urlsafe_base64
+    end
+    
+    code
+  end
+
+  def self.create_short_url(user, long_url)
+    ShortenedUrl.create!(user_id: user.id, long_url: long_url, short_url: self.random_code)
+  end
 end
