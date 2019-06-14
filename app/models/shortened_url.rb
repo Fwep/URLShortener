41aab4c2a8dcd8b3ff4c502535cb3d4fc4ffe064
Taran Cacacho <tarancacacho@gmail.com>
2019-06-14 16:40:37 -0700

    Write class method to prune old/non-premium URLs

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -17,16 +17,39 @@ class ShortenedUrl < ApplicationRecord
     Proc.new { distinct }, 
     through: :visits,
     source: :visitor
-
-  has_many :taggings,
+    
+    has_many :taggings,
     primary_key: :id,
     foreign_key: :url_id,
     class_name: :Tagging
-
-  has_many :tag_topics,
+    
+    has_many :tag_topics,
     through: :taggings,
     source: :tag_topic
-
+    
+    def self.prune(n)
+      ShortenedUrl
+        .joins(:submitter)
+        .joins("LEFT JOIN visits ON visits.url_id = shortened_urls.id")
+        .where(<<-SQL, n.minutes.ago, n.minutes.ago, false)
+          shortened_urls.id IN (
+            SELECT
+              shortened_urls.id
+            FROM
+              shortened_urls
+            JOIN
+              visits ON visits.url_id = shortened_urls.id
+            GROUP BY
+              shortened_urls.id
+            HAVING
+              MAX(visits.created_at) < ?
+          ) OR (visits.id IS NULL AND shortened_urls.created_at > ? )
+            AND
+            users.premium = ?
+        SQL
+        .destroy_all
+    end
+    
   def self.random_code
     code = SecureRandom.urlsafe_base64
 
@@ -52,6 +75,7 @@ class ShortenedUrl < ApplicationRecord
   def num_recent_uniques
     self.visits.select(:user_id).where(" ? > created_at", 10.minutes.ago).distinct.count() # Violating Law of Demeter, I know
   end
+
 
   private
   def no_spamming
